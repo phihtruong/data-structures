82628557f0d5dcc7f947978fc52da59f1bd1e707
Daniel Kim <d95kim@gmail.com>
2021-09-23 17:01:01 -0700

    Completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,15 +3,39 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  //.addToTail() method, takes a value and adds it to the end of the list
   list.addToTail = function(value) {
+    var newTail = Node(value);
+    if (list.tail) {
+      list.tail.next = newTail;
+    }
+    list.tail = newTail;
+    if (list.head === null) {
+      list.head = list.tail;
+    }
   };
-
+  //.removeHead() method, removes the first node from the list and returns its value
   list.removeHead = function() {
+    var head = list.head.value;
+    var nextHead = list.head.next;
+    delete list.head;
+    list.head = nextHead;
+    return head;
   };
 
+  //.contains() method, returns boolean reflecting whether or not the passed-in value is in the linked list
   list.contains = function(target) {
+    var nodeValueList = [];
+    var head = list.head;
+    while (head !== null) {
+      nodeValueList.push(head.value);
+      head = head.next;
+    }
+    if (nodeValueList.includes(target)) {
+      return true;
+    }
+    return false;
   };
-
   return list;
 };
 
