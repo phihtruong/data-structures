cdf75aacc5d36e81d99328637c88abde74f7652d
Daniel Kim <d95kim@gmail.com>
2021-09-24 16:45:02 -0700

    Working on hashTable

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,24 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
+//An .add() method, takes any string and adds it to the set
+// Complexity: O(1)
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
-
+// Complexity: O(n)
 setPrototype.contains = function(item) {
+  return this._storage.includes(item);
 };
-
+// Complexity: O(n)
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item);
+  this._storage.splice(index, 1);
 };
 
 /*
