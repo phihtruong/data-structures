8b4234cd3145109044226303e33c5145b3a34e58
Daniel Kim <d95kim@gmail.com>
2021-09-25 12:43:20 -0700

    Assignment Complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,42 +2,41 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-
+  this.bucket = [];
+  //console.log(this.bucket);
 };
-
+//Complexity: O(1)
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //create a bucket array > push the [k, v]
-  var bucket = [];
-  bucket.push([k, v]);
-  //set this bucket as the v in the set method.
-  this._storage.set(index, bucket);
+  this.bucket.push([k, v]);
+  if (this.bucket[0][0] === k) {
+    this.bucket[0][1] = v;
+  }
+  this._storage.set(index, this.bucket);
 };
 
+//Complexity: O(n)
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
-
-  //return this._storage.get(index);
+  if (this._storage.get(index).length > 1) {
+    //console.log(this._storage.get(index));
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === k) {
+        return this._storage.get(index)[i][1];
+      }
+    }
+  }
+  if (this._storage.get(index)[0]) {
+    return this._storage.get(index)[0][1];
+  }
 };
 
+//Complexity: O(1)
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  //steven is the key and tyler is the value
-  //console.log(this._storage);
   var bucket = this._storage.get(index);
-
-  //console.log(Object.keys(this._storage.get(index)))
-  // for (var i = 0; i < this._storage.length; i++) {
-  //   if (this._storage[i] === k) {
-  //     console.log(this._storage[i])
-  //   }
-  // }
-  //goal: traverse through HashTable using the each method. Find k ('steven')...
-
+  delete this.bucket[0];
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
